16a7addfcdc15964337eaa098d85b4f7e83a39e4
HauroEnojado <victormanu_el08@outlook.com>
2023-06-13 18:24:15 -0300

    configuracion de rutas

--- api/index.js
+++ api/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
 const { PORT } = require('./config')
 const { conn } = require('./src/db/db.js')
+const morgan = require('morgan')
 
 const app = express()
+
+app.use(express.json())
+
+app.use(morgan('dev'))
+
+/*
+  Agregen sus rutas
+*/
 
 conn
   .sync({ force: true })
