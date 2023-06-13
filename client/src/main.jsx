014c8aa9467ee541dffc7141be04e23a3e6a01ff
HauroEnojado <victormanu_el08@outlook.com>
2023-06-13 15:51:45 -0300

    cambios y reorganizacion

--- client/src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-const container = document.getElementById('root')
-const root = ReactDOM.createRoot(container)
-
-root.render(<App />)
