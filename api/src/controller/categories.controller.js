186b08c5c28a621d5467b0c8591ec556ae3add92
Ale <nuala.ni.ale@gmail.com>
2023-06-14 15:41:56 -0500

    controladores para modelos de bases de datos

--- api/src/controller/categories.controller.js
+++ api/src/controller/categories.controller.js
@@ -9,7 +9,7 @@ const getCategories = async (req, res) =>{
             include: {
                 model: Product,
                 attributes: ["idProd","name"]
-
+                
             }
         });
 
@@ -21,6 +21,43 @@ const getCategories = async (req, res) =>{
     }
 }
 
+
+const createCategories = async (req, res) => {
+    try {
+        const {name} = req.body;
+        console.log(name)
+        const allowedCategories = [
+        'electronics',
+        'fashion and accessories',
+        'home and decoration',
+        'sports and fitness / health and wellness',
+        'books and entertainment',
+        'cars and motorcycles',
+        'toys and kids',
+        'personal care',
+        'arts and crafts'
+        ]
+
+        if(!allowedCategories.includes(name)) throw Error(`invalid category: ${name}`)
+
+        const allCategories = await Category.findAll({
+            attributes: ["name"]
+        });
+
+        if (allCategories.find(category => category.name === name)) {
+            throw new Error(`Category ${name} already exists`);
+        }
+
+        const categoryCreated = await Category.create({name});
+
+        
+        res.status(201).json(categoryCreated)
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
+}
+
 module.exports = {
-    getCategories
+    getCategories,
+    createCategories,
 }
