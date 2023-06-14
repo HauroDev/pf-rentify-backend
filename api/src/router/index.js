015b056319e1c8e8f057a1e594c658523efa6045
FabianDP <ingelectronico.fabian@gmail.com>
2023-06-13 20:51:19 -0500

    enlace de rotues con controlador de users

--- api/src/router/index.js
+++ api/src/router/index.js
@@ -3,5 +3,9 @@ const { Router } = require('express')
 const router = Router()
 
 /* pongan su ruta aqui */
+const Users=require('./users.router');
+
+/* routas en use*/
+router.use('/user',Users);
 
 module.exports = router
