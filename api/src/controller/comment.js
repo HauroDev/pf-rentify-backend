51a0f02c449f93d1a93ec3609221d4a5da7a99ac
Gonzalo Llanos <utngonzalollanos@gmail.com>
2023-06-14 10:09:01 -0300

    comments, new file -> new comment, update and destroy

---
+++ api/src/controller/comment.js
@@ -0,0 +1,84 @@
+const { Comment, Product } = require("../db/db");
+
+const { id, comment, puntuation, commentStatus, name } = res.query;
+const newComment = async (req, res) => {
+  try {
+    const newComment = await Comment.create({
+      id,
+      comment,
+      puntuation,
+      commentStatus,
+      name,
+    });
+    await newComment.addPorduct(name);
+    const recordComment = await Comment.findByPk(newComment.id, {
+      include: [
+        {
+          model: Product,
+
+          through: { attributes: [] },
+        },
+      ],
+    });
+    res.json(recordComment);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+};
+
+const updateComment = async (req, res) => {
+  await Comment.update(
+    { id: id },
+    {
+      where: {
+        comment: null,
+      },
+    }
+  );
+};
+
+const destroyComment = async (req, res) => {
+  await Comment.destroy({
+    where: {
+      id: id,
+    },
+  });
+};
+
+module.exports = { newComment, updateComment, destroyComment };
+
+// Definición del modelo de Comentario
+// const Comentario = {
+//   obtenerComentarios: async () => {
+//     const query = 'SELECT * FROM comentarios';
+//     const { rows } = await NewComment.query(query);
+//     return rows;
+//   },
+
+//   obtenerComentarioPorId: async (comentarioId) => {
+//     const query = 'SELECT * FROM comentarios WHERE id = $1';
+//     const values = [comentarioId];
+//     const { rows } = await NewComment.query(query, values);
+//     return rows[0];
+//   },
+
+//   crearComentario: async (contenido, calificacion) => {
+//     const query = 'INSERT INTO comentarios (contenido, calificacion) VALUES ($1, $2) RETURNING *';
+//     const values = [contenido, calificacion];
+//     const { rows } = await NewComment.query(query, values);
+//     return rows[0];
+//   },
+
+//   actualizarComentario: async (comentarioId, contenido, calificacion) => {
+//     const query = 'UPDATE comentarios SET contenido = $1, calificacion = $2 WHERE id = $3 RETURNING *';
+//     const values = [contenido, calificacion, comentarioId];
+//     const { rows } = await NewComment.query(query, values);
+//     return rows[0];
+//   },
+
+//   eliminarComentario: async (comentarioId) => {
+//     const query = 'DELETE FROM comentarios WHERE id = $1';
+//     const values = [comentarioId];
+//     await NewComment.query(query, values);
+//   }
+// };
