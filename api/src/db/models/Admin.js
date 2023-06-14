44cd747c77be2b35b55cdae8bde1c114616479ea
HauroEnojado <victormanu_el08@outlook.com>
2023-06-14 12:23:45 -0300

    actualizando nombres de tablas

--- api/src/db/models/Admin.js
+++ api/src/db/models/Admin.js
@@ -1,19 +1,26 @@
 const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-  sequelize.define('Admin', {
-    idAdmin: {
-      primaryKey: true,
-      type: DataTypes.UUID,
-      allowNull: false
+  sequelize.define(
+    'Admin',
+    {
+      idAdmin: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      }
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    password: {
-      type: DataTypes.STRING,
-      allowNull: false
+    {
+      comment: 'Table containing information about admins',
+      tableName: 'admins'
     }
-  })
+  )
 }
--- api/src/db/models/User.js
+++ api/src/db/models/User.js
@@ -1,34 +1,41 @@
 const { DataTypes } = require('sequelize')
 
 module.exports = (sequelize) => {
-  sequelize.define('User', {
-    idUser: {
-      primaryKey: true,
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      unique: true
+  sequelize.define(
+    'User',
+    {
+      idUser: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        unique: true
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      phone: {
+        type: DataTypes.STRING
+      },
+      image: {
+        type: DataTypes.TEXT, // !cambie
+        allowNull: true // consultar
+      },
+      membership: {
+        type: DataTypes.ENUM(['standard', 'premium']), //
+        allowNull: false
+      },
+      status: {
+        type: DataTypes.ENUM('active', 'inactive', 'paused', 'banned') // !cambie
+      }
     },
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    phone: {
-      type: DataTypes.STRING
-    },
-    image: {
-      type: DataTypes.TEXT, // !cambie
-      allowNull: true // consultar
-    },
-    membership: {
-      type: DataTypes.ENUM(['standard', 'premium']), //
-      allowNull: false
-    },
-    status: {
-      type: DataTypes.ENUM('active', 'inactive', 'paused', 'banned') // !cambie
+    {
+      comment: 'Table containing information about users',
+      tableName: 'users'
     }
-  })
+  )
 }
