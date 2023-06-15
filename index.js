4273ca51e1f02a14a4f7301631854468e3e7248c
HauroEnojado <victormanu_el08@outlook.com>
2023-06-15 14:05:54 -0300

    agregando cors

--- index.js
+++ index.js
@@ -3,12 +3,34 @@ const { PORT } = require('./config')
 const { conn } = require('./src/db/db.js')
 const morgan = require('morgan')
 const router = require('./src/routes/index.js')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 
 const app = express()
 
 app.use(express.json())
-
 app.use(morgan('dev'))
+
+app.name = 'api-rentify'
+
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(cookieParser())
+app.use(morgan('dev'))
+app.use((req, res, next) => {
+  const allowedOrigins = ['http://localhost:5173']
+  const origin = req.headers.origin
+  if (allowedOrigins.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin)
+  }
+  res.header('Access-Control-Allow-Credentials', 'true')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 
 /*
   Agregen sus rutas
