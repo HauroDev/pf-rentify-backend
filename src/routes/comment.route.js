1ad46fe469a3af636d29a4168faeb3bf78b52848
Gonzalo Llanos <utngonzalollanos@gmail.com>
2023-06-16 10:12:04 -0300

    Comentario getIdProduct completo, swagger completo

--- src/routes/comment.route.js
+++ src/routes/comment.route.js
@@ -1,9 +1,91 @@
-const { Router } = require('express')
-const { newComment } = require('../controller/comment.controller.js')
+const { Router } = require("express");
+const {
+  newComment,
+  getCommentsByProductId,
+} = require("../controller/comment.controller.js");
 
-const router = Router()
+const router = Router();
+//schema Comment
+/**
+ * @swagger
+ * components:
+ *   schemas:
+ *     Comment:
+ *       type: object
+ *       properties:
+ *         comment:
+ *           type: string
+ *         puntuation:
+ *           type: integer
+ *           minimum: 1
+ *           maximum: 5
+ *         commentStatus:
+ *           type: boolean
+ *         idProd:
+ *           type: integer
+ *         idUser:
+ *           type: string
+ *           format: uuid
+ *       required:
+ *         - comment
+ *         - puntuation
+ *         - commentStatus
+ *         - idProd
+ *         - idUser
+ */
 
 // metodos post
-router.post('/', newComment)
+/**
+ * @swagger
+ * /comment:
+ *   post:
+ *     summary: Crea un nuevo comentario
+ *     description: Crea un nuevo comentario utilizando los datos proporcionados
+ *     tags:
+ *       - Comentarios
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Comment'
+ *     responses:
+ *       201:
+ *         description: Comentario creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Comment'
+ */
 
-module.exports = router
+router.post("/", newComment);
+
+//Get Commnets product
+/**
+ * @swagger
+ * /comment/{idProduct}:
+ *   get:
+ *     summary: Obtiene todos los comentarios para un producto específico
+ *     description: Obtiene una lista de todos los comentarios asociados a un producto según su ID
+ *     tags:
+ *       - Comentarios
+ *     parameters:
+ *       - in: path
+ *         name: idProduct
+ *         required: true
+ *         description: ID del producto
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: Lista de comentarios obtenida exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Comment'
+ */
+router.get("/:idProduct", getCommentsByProductId);
+
+module.exports = router;
