500783a056631d8a4e190886dcaed9f3f7294fcd
Gonzalo <utngonzalollanos@gmail.com>
2023-06-20 11:52:20 -0300

    Admin Models,Controller, Routes --> index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,13 @@ const productsRoutes = require('./products.route.js')
 const usersRoutes = require('./users.route.js')
 const commentsRoutes = require('./comment.route.js')
 const categoriesRoutes = require('./categories.route.js')
-
+const adminRoutes = require('./Admin.routes.js');
 const router = Router()
 
 router.use('/categories', categoriesRoutes)
 router.use('/user', usersRoutes)
 router.use('/products', productsRoutes)
 router.use('/comment', commentsRoutes)
+router.use('/admin', adminRoutes);
 
 module.exports = router
