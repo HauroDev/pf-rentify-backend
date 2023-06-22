e894975e4968cee82523c1a23b09c080c8c236dd
HauroEnojado <victormanu_el08@outlook.com>
2023-06-22 17:54:17 -0300

    documentacion de /payments

--- src/routes/payments.route.js
+++ src/routes/payments.route.js
@@ -16,11 +16,42 @@ const urlWebHook = urlApi + '/payments/feedback'
  *      properties:
  *        items:
  *          type: array
+ *          items:
+ *            $ref: '#/components/schemas/Item'
  *      required:
  *        - items
  *    Item:
- *      type:
+ *      type: object
+ *      properties:
+ *        title:
+ *          type: string
+ *        unit_price:
+ *          type: number
+ *        quantity:
+ *          type: number
  */
+
+/**
+ * @swagger
+ * /payments/order:
+ *   post:
+ *     summary: Genera la orden de pago
+ *     description: Este endpoint recibe un objeto con una propiedad "items" que es un array que contiene los productos que se están alquilando.
+ *     tags:
+ *       - Payments
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Order'
+ *     responses:
+ *       201:
+ *         description: Orden de pago generada exitosamente.
+ *       400:
+ *         description: Error en la solicitud. Por favor, revise los parámetros enviados.
+ */
+
 router.post('/order', async (req, res) => {
   const { items } = req.body
 
@@ -40,15 +71,46 @@ router.post('/order', async (req, res) => {
     res.status(500).json({ error: error.message })
   }
 })
-
+/**
+ * @swagger
+ * /payments/feedback:
+ *   get:
+ *     summary: Recibe información de feedback
+ *     description: Este webhook recibe información de feedback relacionada con un pago específico.
+ *     tags:
+ *       - Payments
+ *     parameters:
+ *       - in: query
+ *         name: payment_id
+ *         description: ID del pago relacionado con el feedback.
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Información de feedback recibida correctamente.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 payment_id:
+ *                   type: string
+ *                   description: ID del pago relacionado con el feedback.
+ *                   example: "1234567890"
+ */
 router.get(
   '/feedback',
   async (req, res, next) => {
     const { payment_id } = req.query
 
-    const payment = await mp.payment.findById(payment_id)
-    console.log(payment)
-    next()
+    try {
+      const payment = await mp.payment.findById(payment_id)
+      console.log(payment)
+      next()
+    } catch (error) {
+      res.sendStatus(400)
+    }
   },
   (req, res) => {
     console.log(req.query)
