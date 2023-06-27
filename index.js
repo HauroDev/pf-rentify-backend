213286ced732333de15522446ed886195193e1bd
HauroEnojado <victormanu_el08@outlook.com>
2023-06-27 15:18:12 -0300

    pasandolo a release/1.0.0

--- index.js
+++ index.js
@@ -1,71 +1,67 @@
-const express = require("express");
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const cookieParser = require("cookie-parser");
-const swaggerUI = require("swagger-ui-express");
-const swaggerJsDoc = require("swagger-jsdoc");
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const swaggerUI = require('swagger-ui-express')
+const swaggerJsDoc = require('swagger-jsdoc')
 
-const { conn } = require("./src/db/db.js");
-const { PORT, urlApi, urlDoc, URL_CLIENTE } = require("./config");
-const routerManager = require("./src/routes/index.js");
+const { conn } = require('./src/db/db.js')
+const { PORT, urlApi, urlDoc, URL_CLIENTE } = require('./config')
+const routerManager = require('./src/routes/index.js')
 
 const swaggerSpec = {
   definition: {
-    openapi: "3.0.0",
+    openapi: '3.0.0',
     info: {
-      title: "Api-Rentify",
-      version: "1.1.0",
+      title: 'Api-Rentify',
+      version: '1.1.0'
     },
     servers: [
       {
-        url: "http://localhost:3001/api-rentify",
-      },
-    ],
+        url: 'http://localhost:3001/api-rentify'
+      }
+    ]
   },
-  apis: ["./src/routes/*.js"],
-};
+  apis: ['./src/routes/*.js']
+}
 
-const app = express();
+const app = express()
 
-app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.json())
+app.use(morgan('dev'))
 
-app.name = "api-rentify";
+app.name = 'api-rentify'
 
-app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(bodyParser.json({ limit: "50mb" }));
-app.use(cookieParser());
-app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }))
+app.use(bodyParser.json({ limit: '50mb' }))
+app.use(cookieParser())
+app.use(morgan('dev'))
 app.use((req, res, next) => {
-  const allowedOrigins = [URL_CLIENTE, "http://localhost:5173"];
-  const origin = req.headers.origin;
+  const allowedOrigins = [URL_CLIENTE, 'http://localhost:5173']
+  const origin = req.headers.origin
   if (allowedOrigins.includes(origin)) {
-    res.header("Access-Control-Allow-Origin", origin);
+    res.header('Access-Control-Allow-Origin', origin)
   }
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header('Access-Control-Allow-Credentials', 'true')
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  )
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE')
+  next()
+})
 
-app.use("/api-rentify", routerManager);
-app.use(
-  "/api-doc",
-  swaggerUI.serve,
-  swaggerUI.setup(swaggerJsDoc(swaggerSpec))
-);
-console.log(urlDoc + "---> documentacion");
+app.use('/api-rentify', routerManager)
+app.use('/api-doc', swaggerUI.serve, swaggerUI.setup(swaggerJsDoc(swaggerSpec)))
+console.log(urlDoc + '---> documentacion')
 
 conn
   .sync({ force: false })
   .then(() => {
     app.listen(PORT, () =>
-      console.log("Api funcionando en el puerto", PORT, urlApi)
-    );
+      console.log('Api funcionando en el puerto', PORT, urlApi)
+    )
   })
   .catch((error) => {
-    console.error(error);
-  });
+    console.error(error)
+  })
