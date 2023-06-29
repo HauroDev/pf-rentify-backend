a3aaec13a231a4761999d796e716a42733eccef9
HauroEnojado <victormanu_el08@outlook.com>
2023-06-28 22:40:13 -0300

    un mal mergeo, solucionado

--- src/controller/products.controller.js
+++ src/controller/products.controller.js
@@ -4,11 +4,11 @@ const {
   User,
   Comment,
   Country,
-  UserProduct,
-} = require("../db/db");
-const { Op } = require("sequelize");
-const { obtenerNextPageProduct } = require("../utils/paginado.js");
-const { CustomError } = require("../utils/customErrors.js");
+  UserProduct
+} = require('../db/db')
+const { Op } = require('sequelize')
+const { obtenerNextPageProduct } = require('../utils/paginado.js')
+const { CustomError } = require('../utils/customErrors.js')
 
 const getProducts = async (req, res) => {
   // agregar price entre un rango a futuro
@@ -21,52 +21,52 @@ const getProducts = async (req, res) => {
     idCategory,
     idCountry,
     location,
-    state,
-  } = req.query;
+    state
+  } = req.query
 
-  const whereOptions = {};
+  const whereOptions = {}
 
   if (name) {
     whereOptions.name = {
-      [Op.iLike]: `%${name}%`,
-    };
+      [Op.iLike]: `%${name}%`
+    }
   }
 
   if (idCountry) {
-    whereOptions.idCountry = +idCountry;
+    whereOptions.idCountry = +idCountry
     if (state) {
       whereOptions.state = {
-        [Op.iLike]: `%${state}%`,
-      };
+        [Op.iLike]: `%${state}%`
+      }
 
       if (location) {
         whereOptions.location = {
-          [Op.iLike]: `%${location}%`,
-        };
+          [Op.iLike]: `%${location}%`
+        }
       }
     }
   }
 
-  const orderOptions = [["isFeatured", "DESC"]];
+  const orderOptions = [['isFeatured', 'DESC']]
 
-  orderType = orderType || "ASC";
+  orderType = orderType || 'ASC'
 
   switch (orderBy) {
-    case "price":
-      orderOptions.push(["price", orderType]);
-      break;
-    case "name":
-      orderOptions.push(["name", orderType]);
-      break;
-    case "date":
+    case 'price':
+      orderOptions.push(['price', orderType])
+      break
+    case 'name':
+      orderOptions.push(['name', orderType])
+      break
+    case 'date':
     default:
-      orderOptions.push(["updatedAt", orderType]);
+      orderOptions.push(['updatedAt', orderType])
   }
 
   try {
     const result = await Product.findAndCountAll({
       where: {
-        statusPub:  "active"
+        statusPub: 'active'
       },
       whereOptions,
 
@@ -74,200 +74,198 @@ const getProducts = async (req, res) => {
         {
           model: Category,
           through: { attributes: [] },
-          as: "categories",
-          where: idCategory ? { idCategory: +idCategory } : {},
+          as: 'categories',
+          where: idCategory ? { idCategory: +idCategory } : {}
         },
         {
           model: Country,
-          as: "country",
-          attributes: { exclude: ["createdAt", "updatedAt"] },
-        },
+          as: 'country',
+          attributes: { exclude: ['createdAt', 'updatedAt'] }
+        }
       ],
       distinct: true,
       offset: offset || 0,
       limit: limit || 12,
-      order: orderOptions,
-    });
+      order: orderOptions
+    })
 
-    const { rows: products, count } = result;
+    const { rows: products, count } = result
 
-    offset = offset || offset > 0 ? +offset : 0;
-    limit = limit ? +limit : 12;
+    offset = offset || offset > 0 ? +offset : 0
+    limit = limit ? +limit : 12
 
-    let queryExtend = obtenerNextPageProduct(offset, limit, count);
+    let queryExtend = obtenerNextPageProduct(offset, limit, count)
     if (queryExtend) {
-      const params = [];
+      const params = []
 
       if (name) {
-        params.push(`name=${name}`);
+        params.push(`name=${name}`)
       }
 
       if (orderBy) {
-        params.push(`orderBy=${orderBy}`);
+        params.push(`orderBy=${orderBy}`)
       }
 
       if (orderType) {
-        params.push(`orderType=${orderType}`);
+        params.push(`orderType=${orderType}`)
       }
 
       if (idCategory) {
-        params.push(`idCategory=${idCategory}`);
+        params.push(`idCategory=${idCategory}`)
       }
 
       if (idCountry) {
-        params.push(`idCountry=${idCountry}`);
+        params.push(`idCountry=${idCountry}`)
         if (state) {
-          params.push(`state=${state}`);
+          params.push(`state=${state}`)
           if (location) {
-            params.push(`location=${location}`);
+            params.push(`location=${location}`)
           }
         }
       }
 
-      queryExtend += params.length > 0 ? `&${params.join("&")}` : "";
+      queryExtend += params.length > 0 ? `&${params.join('&')}` : ''
     }
 
     res.status(200).json({
       count,
       next: queryExtend,
-      results: products,
-    });
+      results: products
+    })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 const createProduct = async (req, res) => {
-  const { idUser, idCountry, ...product } = req.body;
+  const { idUser, idCountry, ...product } = req.body
 
   try {
     const categoriesDb = await Category.findAll({
-      where: { name: product.categories?.map((cat) => cat.name) },
-    });
+      where: { name: product.categories?.map((cat) => cat.name) }
+    })
     // Validaciones
     if (!categoriesDb.length) {
       throw new CustomError(
         404,
-        "The request could not be completed, Categories not found"
-      );
+        'The request could not be completed, Categories not found'
+      )
     }
 
     if (!idUser) {
       throw new CustomError(
         404,
-        "The request could not be completed, idUser is required to create a product."
-      );
+        'The request could not be completed, idUser is required to create a product.'
+      )
     }
 
-    const user = await User.findByPk(idUser);
+    const user = await User.findByPk(idUser)
 
     if (!user) {
       throw new CustomError(
         404,
-        "The request could not be completed, User is not found."
-      );
+        'The request could not be completed, User is not found.'
+      )
     }
 
-    const country = await Country.findByPk(idCountry);
+    const country = await Country.findByPk(idCountry)
 
     if (!country) {
       throw new CustomError(
         404,
-        "The request could not be completed, Country not found."
-      );
+        'The request could not be completed, Country not found.'
+      )
     }
 
     // fin de Validaciones
 
-    const productDb = await Product.create(product);
+    const productDb = await Product.create(product)
 
-    await productDb.addUser(user);
-    await productDb.addCategories(categoriesDb);
-    await productDb.setCountry(country);
+    await productDb.addUser(user)
+    await productDb.addCategories(categoriesDb)
+    await productDb.setCountry(country)
 
-    let categoriesSearch = await productDb.getCategories();
-    const countrySearch = (await productDb.getCountry()).toJSON();
+    let categoriesSearch = await productDb.getCategories()
+    const countrySearch = (await productDb.getCountry()).toJSON()
 
     categoriesSearch = categoriesSearch.map((cat) => {
       // cada categoria encontrada en la asociacion
       // es un objeto JSON por lo que es necesario
       // para editarlo correctamente hacer esto
-      cat = cat.toJSON();
+      cat = cat.toJSON()
 
-      delete cat.createdAt;
-      delete cat.updatedAt;
-      delete cat.CategoryProduct;
+      delete cat.createdAt
+      delete cat.updatedAt
+      delete cat.CategoryProduct
 
-      return cat;
-    });
+      return cat
+    })
 
-    delete countrySearch.createdAt;
-    delete countrySearch.updatedAt;
+    delete countrySearch.createdAt
+    delete countrySearch.updatedAt
 
     res.status(200).json({
       ...productDb.toJSON(),
       categories: categoriesSearch,
-      country: countrySearch,
-    });
+      country: countrySearch
+    })
   } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: error.message });
+    console.log(error)
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 const getAllProducts = async (req, res) => {
   try {
     // Realiza la consulta para obtener todos los productos
-    const products = await Product.findAll();
+    const products = await Product.findAll()
 
     // Devuelve los productos encontrados como respuesta
-    return res.json(products);
+    return res.json(products)
   } catch (error) {
-    res.status(500).json({ error: "Internal server error" });
+    res.status(500).json({ error: 'Internal server error' })
   }
-};
+}
 
 const getProductById = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
 
   try {
     if (!id) {
       throw new CustomError(
         404,
-        "The request could not be completed, You need an ID to obtain a product."
-      );
+        'The request could not be completed, You need an ID to obtain a product.'
+      )
     }
     const product = await Product.findByPk(id, {
       include: [
-        { model: Category, as: "categories", through: { attributes: [] } },
+        { model: Category, as: 'categories', through: { attributes: [] } },
         {
           model: Country,
-          as: "country",
-          attributes: { exclude: ["createdAt", "updatedAt"] },
-        },
-      ],
-    });
+          as: 'country',
+          attributes: { exclude: ['createdAt', 'updatedAt'] }
+        }
+      ]
+    })
 
     if (!product) {
       throw new CustomError(
         404,
         `The request could not be completed, Product id:${id} is not found`
-      );
+      )
     }
 
     // cambiarlo usando la tabla UserProduct y el type 'owner'
 
     const userOwner = await UserProduct.findOne({
-      where: { [Op.and]: [{ idProd: id }, { type: "owner" }] },
-    });
+      where: { [Op.and]: [{ idProd: id }, { type: 'owner' }] }
+    })
 
-    const infoUser = await User.findByPk(userOwner.idUser);
+    const infoUser = await User.findByPk(userOwner.idUser)
 
     // agregar comentarios
 
     const { count: total, rows: comments } = await Comment.findAndCountAll({
-      where: { idProd: id },
-    });
       where: { idProd: id, commentStatus: true },
       include: [
         {
@@ -298,168 +296,168 @@ const getProductById = async (req, res) => {
       reviews: { total, average, stars: { s1, s2, s3, s4, s5 }, comments }
     })
   } catch (error) {
-    res.status(500).json({ message: error.message });
+    res.status(500).json({ message: error.message })
   }
-};
+}
 
 const getUserProducts = async (req, res) => {
-  const { id } = req.params;
+  const { id } = req.params
   try {
-    const user = await User.findByPk(id);
-    console.log(user);
+    const user = await User.findByPk(id)
+    console.log(user)
     if (!user) {
-      throw new Error(404, "User not valid");
+      throw new Error(404, 'User not valid')
     }
 
     const products = await user.getProducts({
-      include: [{ model: Category, as: "categories" }],
-      through: { attributes: [] },
-    });
-    res.status(200).json(products);
+      include: [{ model: Category, as: 'categories' }],
+      through: { attributes: [] }
+    })
+    res.status(200).json(products)
   } catch (error) {
     // Manejar errores de consulta
-    console.error("Error al obtener los productos del usuario:", error);
-    throw error;
+    console.error('Error al obtener los productos del usuario:', error)
+    throw error
   }
-};
+}
 
 // Controlador para actualizar el statusProd de un producto
 const updateProductstatusPub = async (req, res) => {
-  const { idProd, statusPub } = req.body; // Suponiendo que recibes el ID del producto como parámetro en la URL
-  console.log(idProd);
+  const { idProd, statusPub } = req.body // Suponiendo que recibes el ID del producto como parámetro en la URL
+  console.log(idProd)
   try {
     // Buscar el producto por su ID
-    const product = await Product.findByPk(idProd);
-    console.log(product);
+    const product = await Product.findByPk(idProd)
+    console.log(product)
     if (!product) {
-      return res.status(404).json({ error: "Product not found ??" });
+      return res.status(404).json({ error: 'Product not found ??' })
     }
 
     // Validar que statusPub sea uno de los valores permitidos
-    const allowedStatus = ["active", "inactive", "delete"];
+    const allowedStatus = ['active', 'inactive', 'delete']
     if (!allowedStatus.includes(statusPub)) {
       return res.status(400).json({
         error:
-          "Invalid value for statusPub. It must be one of: active, inactive, delete",
-      });
+          'Invalid value for statusPub. It must be one of: active, inactive, delete'
+      })
     }
     // Validar que el estado no sea igual al valor actual en la base de datos
     if (statusPub === product.statusPub) {
       return res
         .status(400)
-        .json({ error: "Status is already set to the provided value" });
+        .json({ error: 'Status is already set to the provided value' })
     }
 
     // Actualizar el statusProd del producto
-    product.statusPub = statusPub;
-    await product.save();
+    product.statusPub = statusPub
+    await product.save()
 
     return res
       .status(200)
-      .json({ message: "Product status updated successfully" });
+      .json({ message: 'Product status updated successfully' })
   } catch (error) {
-    console.error("Error updating product status:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    console.error('Error updating product status:', error)
+    return res.status(500).json({ error: 'Internal server error' })
   }
-};
+}
 
 const updateProductName = async (req, res) => {
-  const { idProd, name } = req.body;
+  const { idProd, name } = req.body
 
   try {
     // Buscar el producto por su ID
-    const product = await Product.findByPk(idProd);
+    const product = await Product.findByPk(idProd)
 
     if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      return res.status(404).json({ error: 'Product not found' })
     }
 
     // Actualizar el nombre del producto
-    product.name = name;
-    await product.save();
+    product.name = name
+    await product.save()
 
     return res
       .status(200)
-      .json({ message: "Product name updated successfully" });
+      .json({ message: 'Product name updated successfully' })
   } catch (error) {
-    console.error("Error updating product name:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    console.error('Error updating product name:', error)
+    return res.status(500).json({ error: 'Internal server error' })
   }
-};
+}
 
 const updateProductPrice = async (req, res) => {
-  const { idProd, price } = req.body;
+  const { idProd, price } = req.body
   if (isNaN(price)) {
-    return res.status(400).json({ error: "Price must be a number" });
+    return res.status(400).json({ error: 'Price must be a number' })
   }
 
   try {
     // Buscar el producto por su ID
-    const product = await Product.findByPk(idProd);
+    const product = await Product.findByPk(idProd)
 
     if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      return res.status(404).json({ error: 'Product not found' })
     }
 
     // Actualizar el precio del producto
-    product.price = price;
-    await product.save();
+    product.price = price
+    await product.save()
 
     return res
       .status(200)
-      .json({ message: "Product price updated successfully" });
+      .json({ message: 'Product price updated successfully' })
   } catch (error) {
-    console.error("Error updating product price:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    console.error('Error updating product price:', error)
+    return res.status(500).json({ error: 'Internal server error' })
   }
-};
+}
 
 const updateProductIsFeatured = async (req, res) => {
-  const { idProd, isFeatured } = req.body;
+  const { idProd, isFeatured } = req.body
 
   try {
-    const product = await Product.findByPk(idProd);
+    const product = await Product.findByPk(idProd)
     if (!product) {
-      return res.status(404).json({ error: "Product not found" });
+      return res.status(404).json({ error: 'Product not found' })
     }
-    if (typeof isFeatured !== "boolean") {
+    if (typeof isFeatured !== 'boolean') {
       return res
         .status(400)
-        .json({ error: "Invalid value for isFeatured. It must be a boolean." });
+        .json({ error: 'Invalid value for isFeatured. It must be a boolean.' })
     }
     // Actualizar el campo isFeatured del producto
-    product.isFeatured = isFeatured;
-    await product.save();
+    product.isFeatured = isFeatured
+    await product.save()
 
     return res
       .status(200)
-      .json({ message: "Product isFeatured updated successfully" });
+      .json({ message: 'Product isFeatured updated successfully' })
   } catch (error) {
-    console.error("Error updating product isFeatured:", error);
-    return res.status(500).json({ error: "Internal server error" });
+    console.error('Error updating product isFeatured:', error)
+    return res.status(500).json({ error: 'Internal server error' })
   }
-};
+}
 
 const getProductByFeature = async (req, res) => {
   try {
-    const { isFeatured } = req.query; // Obtén el parámetro de consulta 'feature'
-    console.log(isFeatured);
+    const { isFeatured } = req.query // Obtén el parámetro de consulta 'feature'
+    console.log(isFeatured)
 
     // Realizar la búsqueda de productos por característica
     const products = await Product.findAll({
       where: {
-        isFeatured, // Filtrar por la característica proporcionada
-      },
-    });
+        isFeatured // Filtrar por la característica proporcionada
+      }
+    })
 
-    return res.status(200).json(products);
+    return res.status(200).json(products)
   } catch (error) {
-    console.log(error);
+    console.log(error)
     return res
       .status(500)
-      .json({ error: "Error en la búsqueda de productos por característica" });
+      .json({ error: 'Error en la búsqueda de productos por característica' })
   }
-};
+}
 
 module.exports = {
   getProducts,
@@ -471,5 +469,5 @@ module.exports = {
   updateProductName,
   updateProductPrice,
   updateProductIsFeatured,
-  getProductByFeature,
-};
+  getProductByFeature
+}
