e025a0acb6b1c2b0f494bcf54701ef38221ea1b3
gonlla8777 <utngonzalollanos@gmail.com>
2023-06-29 17:32:21 -0300

    comenzando jwt

---
+++ src/utils/errorToken.js
@@ -0,0 +1,16 @@
+const errorsToken = (message) => {
+  const errors = {
+    ["jwt malformed"]: "Invalid token format",
+    ["invalid token"]: "invalid token",
+    ["jwt expired"]: "expired token",
+    ["invalid signature"]: "The token signature is not valid",
+    ["No Bearer"]: "Use the Bearer format",
+    ["No token"]: "No token sent",
+  };
+
+  return errors[message]
+    ? errors[message]
+    : "An unexpected error occurred in token validation";
+};
+
+module.exports = { errorsToken };
