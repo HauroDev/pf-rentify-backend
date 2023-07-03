cb46ef61e436272738f7400d129cd54b9332e237
HauroEnojado <victormanu_el08@outlook.com>
2023-07-02 21:20:05 -0300

    arreglando modos

--- src/controller/contact.controller.js
+++ src/controller/contact.controller.js
@@ -1,57 +1,57 @@
-const { sendContactEmail, sendOwnerEmail } = require('../config/nodemailer');
+const { sendContactEmail, sendOwnerEmail } = require('../config/nodemailer')
 
 const contactOwner = async (req, res) => {
   try {
-    const { emailUser, nameUser, ownerEmail, product } = req.body;
-    console.log('userEmail:', emailUser);
-    console.log('userName:', nameUser);
-    console.log('ownerEmail:', ownerEmail);
-    console.log('product:', product);
+    const { emailUser, nameUser, ownerEmail, product } = req.body
+    console.log('userEmail:', emailUser)
+    console.log('userName:', nameUser)
+    console.log('ownerEmail:', ownerEmail)
+    console.log('product:', product)
 
     if (!emailUser || !nameUser || !ownerEmail || !product) {
-      return res.status(400).json({ error: 'Required fields are missing' });
+      return res.status(400).json({ error: 'Required fields are missing' })
     }
 
-    const emailRegex = /^\S+@\S+\.\S+$/;
+    const emailRegex = /^\S+@\S+\.\S+$/
     if (!emailRegex.test(emailUser)) {
-      return res.status(400).json({ error: 'Invalid email format' });
+      return res.status(400).json({ error: 'Invalid email format' })
     }
 
     // Nodemailer
-    await sendOwnerEmail(nameUser, emailUser, ownerEmail, product);
+    await sendOwnerEmail(nameUser, emailUser, ownerEmail, product)
 
-    res.json({ success: true });
+    res.json({ success: true })
   } catch (error) {
-    console.log('Error sending the email:', error);
-    res.status(500).json({ error: 'Error sending the email' });
+    console.log('Error sending the email:', error)
+    res.status(500).json({ error: 'Error sending the email' })
   }
-};
+}
 
 const contactUs = async (req, res) => {
   try {
-    const { name, email, message } = req.body;
+    const { name, email, message } = req.body
 
-    console.log('name:', name);
-    console.log('email:', email);
-    console.log('message:', message);
+    console.log('name:', name)
+    console.log('email:', email)
+    console.log('message:', message)
 
     if (!name || !email || !message) {
-      return res.status(400).json({ error: 'Required fields are missing' });
+      return res.status(400).json({ error: 'Required fields are missing' })
     }
 
-    const emailRegex = /^\S+@\S+\.\S+$/;
+    const emailRegex = /^\S+@\S+\.\S+$/
     if (!emailRegex.test(email)) {
-      return res.status(400).json({ error: 'Invalid email format' });
+      return res.status(400).json({ error: 'Invalid email format' })
     }
 
     // Nodemailer
-    await sendContactEmail(name, email, message);
+    await sendContactEmail(name, email, message)
 
-    res.json({ success: true });
+    res.json({ success: true })
   } catch (error) {
-    console.log('Error sending the email:', error);
-    res.status(500).json({ error: 'Error sending the email' });
+    console.log('Error sending the email:', error)
+    res.status(500).json({ error: 'Error sending the email' })
   }
-};
+}
 
-module.exports = { contactOwner, contactUs };
+module.exports = { contactOwner, contactUs }
