fcdbe2f0826ff0784f350ef697203e2a9bdc5c3b
HauroEnojado <victormanu_el08@outlook.com>
2023-07-03 18:06:55 -0300

    cambiando confirmacion del commentStatus

--- src/utils/errorToken.js
+++ src/utils/errorToken.js
@@ -1,16 +1,17 @@
+/* eslint-disable no-useless-computed-key */
 const errorsToken = (message) => {
   const errors = {
-    ["jwt malformed"]: "Invalid token format",
-    ["invalid token"]: "invalid token",
-    ["jwt expired"]: "expired token",
-    ["invalid signature"]: "The token signature is not valid",
-    ["No Bearer"]: "Use the Bearer format",
-    ["No token"]: "No token sent",
-  };
+    ['jwt malformed']: 'Invalid token format',
+    ['invalid token']: 'invalid token',
+    ['jwt expired']: 'expired token',
+    ['invalid signature']: 'The token signature is not valid',
+    ['No Bearer']: 'Use the Bearer format',
+    ['No token']: 'No token sent'
+  }
 
   return errors[message]
     ? errors[message]
-    : "An unexpected error occurred in token validation";
-};
+    : 'An unexpected error occurred in token validation'
+}
 
-module.exports = { errorsToken };
+module.exports = { errorsToken }
--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -1,24 +1,24 @@
-const jwt = require("jsonwebtoken");
-const { JWT_SECRET, MODE } = process.env;
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET, MODE } = process.env
 
 const generateToken = (userId, userEmail, role, res) => {
   try {
-    const expiresIn = 60 * 60 * 24 * 7; // 7 dias
+    const expiresIn = 60 * 60 * 24 * 7 // 7 dias
     const token = jwt.sign({ userId, userEmail, role }, JWT_SECRET, {
-      expiresIn: expiresIn,
-    });
+      expiresIn
+    })
 
-    res.cookie("auth_token", token, {
+    res.cookie('auth_token', token, {
       httpOnly: true, // La cookie solo puede ser leída por el servidor
-      secure: !(MODE === "development"), // solo es seguro cuando esta en produccion
-      expires: new Date(Date.now() + expiresIn * 1000),
-    });
+      secure: !(MODE === 'development'), // solo es seguro cuando esta en produccion
+      expires: new Date(Date.now() + expiresIn * 1000)
+    })
 
-    return { token, expiresIn };
+    return { token, expiresIn }
   } catch (error) {
-    const status = error.status || 500;
-    res.status(status).json({ error: error.message });
+    const status = error.status || 500
+    res.status(status).json({ error: error.message })
   }
-};
+}
 
-module.exports = { generateToken };
+module.exports = { generateToken }
